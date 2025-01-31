9731f5f44419273f6b1a6f77aab9160fa322693e
Elton-Sudo <eltondonald077@gmail.com>
2025-01-31 21:35:40 +0200

    chore(Utils): Create and implement a logger

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,40 +1,40 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
 import { HttpStatusCode } from '../utils/enum/httpStatusCode.enum';
+import { verifyToken } from '../utils/jwt.util';
+import logger from '../utils/logger'; // Fixed import path
 
-dotenv.config();
-
-const secretKey = process.env.SECRET_KEY as string;
-
-export default function authMiddleware(requireAuth: boolean = true) {
-  return (req: Request, res: Response, next: NextFunction): void => {
-    if (!requireAuth) {
-      // Skip authentication check if requireAuth is false
-      return next();
-    }
-
-    const token = req.header('Authorization')?.replace('Bearer ', '');
-
-    if (!token) {
-      res.status(HttpStatusCode.UNAUTHORIZED).json({
-        success: false,
-        message: 'Access denied. No token provided.',
-      });
-    }
-
+export default function authMiddleware(
+  requireAuth: boolean = true,
+): (req: Request, res: Response, next: NextFunction) => void {
+  return (req: Request, res: Response, next: NextFunction): Response | void => {
     try {
-      // Verify and decode the JWT token
-      const decoded = jwt.verify(token, secretKey);
+      if (!requireAuth) {
+        return next();
+      }
 
-      // Attach the decoded user info to the request object
+      const token = req.header('Authorization')?.replace('Bearer ', '');
+      if (!token) {
+        logger.error('Unauthorized Access Attempt: No token provided.');
+        return res.status(HttpStatusCode.UNAUTHORIZED).json({
+          success: false,
+          message: 'Access denied. No token provided.',
+        });
+      }
+
+      const decoded = verifyToken(token);
+      if (!decoded) {
+        logger.error('Unauthorized Access Attempt: Invalid or expired token.');
+        return res.status(HttpStatusCode.UNAUTHORIZED).json({
+          success: false,
+          message: 'Invalid or expired token.',
+        });
+      }
+
       req.user = decoded;
       next();
     } catch (error) {
-      res.status(HttpStatusCode.UNAUTHORIZED).json({
-        success: false,
-        message: 'Invalid or expired token.',
-      });
+      logger.error(`Authentication Middleware Error: ${error}`);
+      next(error); 
     }
   };
 }
